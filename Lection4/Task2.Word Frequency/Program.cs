12fb618f5b5ca91b450d9e970b96097d7364628a
Stepan <31258215+StepanTuchin98@users.noreply.github.com>
2018-12-10 15:36:46 +0400

    Second task

---
+++ Lection4/Task2.Word Frequency/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Word_Frequency
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the text with seperators like gap and dot.");
+            string text = Console.ReadLine();
+
+            var wordFrequencyCount = new Dictionary<string, int>();
+            var words = text.Split(' ', '.').Where(x => x.Length > 0);
+
+            foreach (var word in words)
+            {
+                if (!wordFrequencyCount.ContainsKey(word))
+                {
+                    wordFrequencyCount.Add(word, 1);
+                }
+                else
+                {
+                    wordFrequencyCount[word]++;
+                }
+            }
+
+            Console.WriteLine("Words frequency:\n");
+
+            foreach (var word in wordFrequencyCount)
+            {
+                Console.WriteLine("{0} - {1} times", word.Key, word.Value);
+            }
+        }
+    }
+}
