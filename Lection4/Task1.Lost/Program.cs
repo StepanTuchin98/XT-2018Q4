b76f4d611ae27c2bb542e015e31aa5b433efcb61
Stepan <31258215+StepanTuchin98@users.noreply.github.com>
2018-12-10 15:07:32 +0400

    first Task

---
+++ Lection4/Task1.Lost/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Lost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (true)
+            {
+                Console.WriteLine("Enter n - numbers of people. positive");
+
+                if (int.TryParse(Console.ReadLine(), out n))
+                {
+                    if (n > 0)
+                    {
+                        break;
+                    }
+                }
+            }
+            
+            var people = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                people.Add(i);
+            }
+
+            GetLost(ref people);
+            Console.WriteLine(people[0]);
+        }
+
+        public static void GetLost(ref List<int> people)
+        {
+            int it = 1;
+            while (people.Count > 1)
+            {
+                it = it % people.Count;
+                people.RemoveAt(it++);
+            }
+        }
+    }
+}
