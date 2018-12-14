2f9d42028d9f44e4d9b303e5ec3526d9fa1c9dcd
Stepan <31258215+StepanTuchin98@users.noreply.github.com>
2018-12-14 21:27:04 +0400

    Abstract/Figure.cs

---
+++ Lection3/Epam.StudentPractice.Lection3.Task7.VectorGraphicEditor/Abstract/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.StudentPractice.Lection3.Task7.VectorGraphicEditor
+{
+    abstract class Figure
+    {
+        public abstract void Show();
+
+        protected Figure()
+        {
+
+        }
+    }
+}
