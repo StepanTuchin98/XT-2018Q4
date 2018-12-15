c061a6c58c9175a00ba346192b4994dece858911
Stepan <31258215+StepanTuchin98@users.noreply.github.com>
2018-12-15 19:58:37 +0400

    I think, that generics will be much better hear...

---
+++ Lection5/Task4.NumberArraySum/ArrayHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.NumberArraySum
+{
+    public static class ArrayHelper
+    {
+        public static int Sum(this int[] arr)
+        {
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static double Sum(this double[] arr)
+        {
+            double sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+        public static long Sum(this long[] arr)
+        {
+            long sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static float Sum(this float[] arr)
+        {
+            float sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+
+        public static decimal Sum(this decimal[] arr)
+        {
+            decimal sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Lection5/Task4.NumberArraySum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.NumberArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 5, 3, 0, 53, -14, 42, 2, -45 };
+            int sum = arr.Sum();
+            Console.WriteLine($"Summary of array: {sum}");
+        }
+    }
+}
