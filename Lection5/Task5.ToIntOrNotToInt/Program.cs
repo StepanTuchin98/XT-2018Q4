b82e3bc8275e101c205f543a531ccdba4a78e2a1
Stepan <31258215+StepanTuchin98@users.noreply.github.com>
2018-12-16 23:07:32 +0400

    last

---
+++ Lection5/Task5.ToIntOrNotToInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.ToIntOrNotToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = Console.ReadLine();
+            if (test.IsPositiveInt())
+            {
+                Console.WriteLine("Positive number.");
+            }else
+            {
+                Console.WriteLine("Not positive number.");
+            }
+        }
+    }
+}
---
+++ Lection5/Task5.ToIntOrNotToInt/StringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task5.ToIntOrNotToInt
+{
+    public static class StringHelper
+    {
+        public static bool IsPositiveInt(this string str)
+        {
+            var pattern = new Regex(@"(\d*\,|\.*\d+E[+,-]\d+)|(\d*\,|\.*\d+)");
+            return pattern.IsMatch(str) & pattern.Replace(str, String.Empty) == String.Empty;
+        }
+    }
+}
